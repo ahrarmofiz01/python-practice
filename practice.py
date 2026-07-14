e87fd9a06d0e9c6316c0517a21288726bf597418
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-14 21:18:02 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -6,19 +6,10 @@ while count<=5:
     count=count+1
 print(num)
 #list
-list=["apple","banana","cherry","orange","kiwi"]
-print(list)
-num=[10,20,30]
-print(num)
-num.append(40)
-print(num)
-num.insert(0,5) #Add 5 at the beginning.
-print(num)
-num.remove(20)
-print(num)
 #find sum and avrage
 marks=[10,20,30,40,50]
 total=sum(marks)
 print(total)
 avrage=total/len(marks)
 print(avrage)
+number=[1,2,3,4]
