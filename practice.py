fc231d2a61ff774230882835e83c5c4fd21b4f04
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-17 22:09:47 +0530

    add code

--- practice.py
+++ practice.py
@@ -51,13 +51,17 @@ for items in items:
 def myfunction():
     print("hello world")
 myfunction()
+
 def add_function(a,b):
     return a+b
 
 result = add_function(10,20)
 print(result)
+
 def mathfunction(a,b):
     return a*b
 result=mathfunction(10,20)
 result=mathfunction(10,300)
 print(result)
+input1=12345
+print(input1%10)
