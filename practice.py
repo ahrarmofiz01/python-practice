ae45ab9f8cb9655d92e1b23e89eddc8b572005e3
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-18 22:50:49 +0530

    add code

--- practice.py
+++ practice.py
@@ -6,3 +6,17 @@ if average > 80 :
     print(name,"nice you are eligble")
 else:
     print("better luck next time")
+
+add1 = input("Enter fruit 1: ")
+add2 = input("Enter fruit 2: ")
+add3 = input("Enter fruit 3: ")
+add4 = input("Enter fruit 4: ")
+
+fruits = ["apple", "banana", "orange", "kiwi"]
+
+user_list = [add1, add2, add3, add4]
+
+if user_list == fruits:
+    print("Correct! The lists are the same.")
+else:
+    print("Wrong! The lists are not the same.")
