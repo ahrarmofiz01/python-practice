44151f02f62a7c82a02cbc2e67a9cd0765d05dc9
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-18 21:57:19 +0530

    add file

---
+++ practice.py
@@ -0,0 +1,8 @@
+name=input("enter student name:")
+marks=[80,90,87,78]
+print(sum(marks)/4)
+average=sum(marks)/4
+if average > 80 :
+    print(name,"nice you are eligble")
+else:
+    print("better luck next time")
