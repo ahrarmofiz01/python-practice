9d3e446ead8ddba860f368c81868d7ce4cb80900
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-18 21:10:50 +0530

    add code

---
+++ classes.py
@@ -0,0 +1,10 @@
+class person:
+    def __init__(self,name):
+        self.name = name
+p1=person("ahrar")
+print(p1.name)
+class contact:
+    def __init__(self,email):
+        self.email=email
+c1=contact("ahrar@example.com")
+print(c1.email)
