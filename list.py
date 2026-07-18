1a32102e3ae2a72bebcf203a114abc34415ccf73
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-18 20:55:57 +0530

    add file

--- list.py
+++ list.py
@@ -47,26 +47,7 @@ while i<len(name):
 items=["apple","orrange"]
 for items in items:
     print(items,end=" ")
-# function
-def myfunction():
-    print("hello world")
-myfunction()
 
-def add_function(a,b):
-    return a+b
 
-result = add_function(10,20)
-print(result)
-
-def mathfunction(a,b):
-    return a*b
-result=mathfunction(10,20)
-result=mathfunction(10,300)
-print(result)
-input1=12345
-print(input1%10)
-def function(a):
-    print(3.14*a*a)
-function(3)
 
     
