01e8110fc16279843aeb99a9ddc20db61a286779
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-19 16:00:04 +0530

    adding code

--- practice.py
+++ practice.py
@@ -3,3 +3,16 @@ if num%2==0:
     print("num is even")
 else:
     print("num is odd")
+num1=int(input("enter your nnum:"))
+if num >0:
+    print("num is positive")
+elif num==0:
+    print("nnum is zero")
+else:
+    print("num is negative")
+number1=int(input("enter your number1:"))
+number2=int(input("enter your number1:"))
+if number1>number2:
+    print(number1 ,"is greatere then", number2)
+else:
+    print(number2,"is greater then",number1)
