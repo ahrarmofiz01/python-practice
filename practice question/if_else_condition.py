3befb5513ffa4a97127343385173becb6585f42f
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-19 22:10:02 +0530

    add code

--- practice question/if_else_condition.py
+++ practice question/if_else_condition.py
@@ -8,6 +8,19 @@ elif number2 > number1 and number2 > number3:
     print(number2, "is the largest number")
 else:
     print(number3, "is the largest number")
+#all number are equal
+number1 = int(input("Enter your number1: "))
+number2 = int(input("Enter your number2: "))
+number3 = int(input("Enter your number3: "))
 
+if number1 > number2 and number1 > number3:
+    print(number1, "is the largest number")
 
-    
+elif number2 > number1 and number2 > number3:
+    print(number2, "is the largest number")
+
+elif number3 > number1 and number3 > number2:
+    print(number3, "is the largest number")
+
+else:
+    print("All numbers are equal or at least two numbers are equal.")
