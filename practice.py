453b34e792ab462afd54794ad4f8527a307a3dc0
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-19 15:04:02 +0530

    add num

--- practice.py
+++ practice.py
@@ -1,18 +1,5 @@
-name=input("enter student name:")
-marks=[80,90,87,78]
-print(sum(marks)/4)
-average=sum(marks)/4
-if average > 80 :
-    print(name,"nice you are eligble")
+num=int(input("enter your num:"))
+if num%2==0:
+    print("num is even")
 else:
-    print("better luck next time")
-
-add1=input("enter your list:")
-add2=input("enter your input:")
-add3=input("enter your input:")
-list=["apple","banana","orange"]
-cart_list=[add1,add2,add3]
-if list==cart_list:
-    print("done")
-else:
-    print("not done")
+    print("num is odd")
