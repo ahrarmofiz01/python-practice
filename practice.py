d9806ea3a1708aa6eb7d378a2c37570f9cdd4b3b
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-19 16:40:29 +0530

    add code

---
+++ practice.py
@@ -0,0 +1,12 @@
+number1=int(input("enter your number1:"))
+number2=int(input("enter your number2:"))
+number3=int(input("enter your number3:"))
+if number1> number2 and number1>number3:
+    print(number1 ,"is largest number" )
+elif number2>number3 and number3>number1:
+    print(number2 ,"is larger number")
+else :
+    print(number3 , "largest number")
+
+
+    
