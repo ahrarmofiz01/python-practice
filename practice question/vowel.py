a2f2c2a7600d50899b205ad4067ac6fe649e06e4
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-20 21:56:53 +0530

    add code

---
+++ practice question/vowel.py
@@ -0,0 +1,5 @@
+letter=input("input your first letter")
+if letter=="a"or letter=="A"or letter=="e"or letter=="E" or letter=="i" or letter=="I" or letter=="o" or letter=="O" or letter=="u" or letter=="U":
+    print("letter is volel")
+else:
+    print("this is consunent")
