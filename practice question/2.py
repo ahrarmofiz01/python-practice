9e0e44a6b040a662bda7e5930a5302f02024de8c
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-20 21:40:42 +0530

    add code

--- practice question/2.py
+++ practice question/2.py
@@ -7,3 +7,13 @@ elif year%4==0:
     print(year,"is a leap year")
 else:
     print("not a leap year")
+unit=int(input("enter the unit:"))
+if unit <=100:
+    print(unit*5)
+elif unit>100 and unit<=200:
+    print(unit*7)
+elif unit>200 and unit >=1000:
+    print(unit*10)
+else:
+    print("go to goverment office your bill is too high")
+    
