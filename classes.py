fb3b67521477075b6a257ad1ebceb2f552119dfc
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-22 21:39:58 +0530

    add code

--- classes.py
+++ classes.py
@@ -4,3 +4,10 @@ class name:
 p1 =name("ahrar")
 print(p1.name)
 #A function is used to perform a specific task. A class is used to create objects that have both data (variables) and behavior (methods). Functions are defined with def, while classes are defined with class.
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=int(age)
+a1=student("ahrar",20)
+print(a1.name)
+print(a1.age)
