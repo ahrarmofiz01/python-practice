c8bf7fdec79424062164e002608eb2db5dfdc0ce
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-23 15:16:59 +0530

    add code

--- practice question/atm.py
+++ practice question/atm.py
@@ -1,4 +1,14 @@
+currentblance=10000
 password=1234
 pin=int(input("enter your pin:"))
 if pin==password:
-    print("password is coreect")
+    print("your password is correct")
+else:
+    print("wrong password")
+
+balance=100000
+creditammount=int(input("enter your ammount"))
+if creditammount<=balance:
+    print(balance-creditammount,"this is your ablable balance")
+elif creditammount>balance:
+    print("incuficent balance")
