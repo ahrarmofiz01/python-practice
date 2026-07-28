e83f02c9d34f2b797eab60753288bd5f4eb9db76
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-28 21:58:37 +0530

    add code

---
+++ %.py
@@ -0,0 +1,10 @@
+n=int(input("enter thr number:"))
+list=[]
+for i in range(1,n+1):
+    if i%3==0 and i%5==0:
+        list.append("fizzbuzz")
+    elif i%3==0:
+        list.append("fizz")
+    else:
+        list.append(str(i))
+print(list)
--- whileloops.py
+++
@@ -1,4 +0,0 @@
-list=[10,25,15,8,20]
-list.sort()
-print(list)
-print(list[-2])
