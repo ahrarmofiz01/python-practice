5c3a4d4c2473cbf71341c2ae16ead29965ba8507
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-30 22:03:19 +0530

    add code

--- star.py
+++ star.py
@@ -1,4 +1,33 @@
-for i in range(10, 0, -1):
-    for j in range(i):
-        print("*", end="")
-    print()
+for i in  range(1,11):
+    print(i)
+n=10
+if n%2==0:
+    print("n is even")
+else:
+    print("odd")
+
+count=0
+while count<=100:
+    print(count)
+    count=count+2
+for i in range(1,11):
+    print(i*7)
+num=1
+count=1
+while count<=5:
+    num=num*count
+    count=count+1
+print(num)
+n=7
+for i in range(1,11):
+    print(n,"x",i,"=",n*i)
+number=12
+for i in range(1,11):
+    if 12%i==0:
+        print(i,"this is the factor")
+list=[1,9,8,7]
+largest=list[0]
+for i in list:
+    if i>largest:
+        largest=i
+print(largest)
