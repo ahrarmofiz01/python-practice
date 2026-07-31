41da32e2c5afe1cfbf25f7fdd63baedb20687b42
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-31 21:36:50 +0530

    add code

--- practice question/socialmediya basics.py
+++ practice question/socialmediya basics.py
@@ -20,5 +20,13 @@ print("100" > "99")
 a=[1,2,3,4,5]
 print(a*2)
 b=[1,2,3,4,5]
-print(b*0)
+print(b*0)#ye concept main hmne sikhaa zero ko chod kr baaki kisi v number say multiply krte hai tb utnee time repit hota hai likne zero say multiply krne say ek null list milta hai
+nums=[12,5,8,7,20]
+list=[]
+for i in nums:
+    if i%2==1:
+        list.append(i)
+print(list)
+print(list[0]+list[1])
+
 
