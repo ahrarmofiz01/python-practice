61287a3e4a38f7f8c7b4b9cdd44698c2af7b8119
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-04 20:50:31 +0530

    add code

---
+++ practice question/search.py
@@ -0,0 +1,27 @@
+nums = [5,8,2,15,7]
+
+found = False
+
+for i in nums:
+    if i == 7:
+        found = True
+
+print(found)
+nums = [5,8,2,15,7]
+found=False
+for i in nums:
+    if i == 100:
+        found = True
+print(found)
+nums = [12,5,18,7,20]
+found=False
+for i in nums:
+    if i>15 and i%2==0:
+        found=True
+print(found)
+nums = [4, 9, 12, 15, 18]
+found=False
+for i in nums:
+    if i<10 and i%2==1:
+        found=True
+print(found)
