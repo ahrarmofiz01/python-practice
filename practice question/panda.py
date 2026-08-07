c852648fce38dd69ec010d126ee596c4864f8633
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-07 20:41:32 +0530

    add code

--- practice question/panda.py
+++ practice question/panda.py
@@ -1,25 +1,4 @@
 import pandas as pd
-pd.__version__
-df=pd.DataFrame([1,2,3],columns=['column_number'])
-print(type(df))
+df = pd.read_csv("/Users/ahrarmofiz/Downloads/archive (6)/customer_churn_dataset-testing-master.csv")
 print(df)
-data={
-    'name':['alice','bob','charlie'],
-    'age':[25,30,35],
-    'salary':[50000,60000,70000]
-}
-df=pd.DataFrame(data)
-print(df)
-print(df.head(1))#top rows
-print(df.shape)
-print(df.columns)
-df.rename(columns={'name': 'NAME'},inplace=True)
-print(df.NAME)
-print(df)
-print(df.info)
-print(df.describe())
-#save and load data from csv
-df.to_csv('test_data.csv')
-df=pd.read_csv('test_data.csv')
-print(df[['NAME']])
-print(df.loc[df.salary>=50000])
+print(df.head(10))
