3039d1906a5ac5eeb040c78554aeaf087b07e733
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-07 20:59:18 +0530

    add code

--- practice question/panda.py
+++ practice question/panda.py
@@ -1,6 +1,9 @@
 import pandas as pd
 df = pd.read_csv("/Users/ahrarmofiz/Downloads/archive (6)/customer_churn_dataset-testing-master.csv")
-print(df)
-print(df.head(10))#top 10 rows
-print(df.tail(5))#last 5 rows dikhata hai.
-print(df.shape)
+#print(df)
+#print(df.head(10))#top 10 rows
+#print(df.tail(5))#last 5 rows dikhata hai.
+print(df.shape)#DataFrame me kitni rows aur kitne columns hain.
+print(df.columns)#saare column ke naam
+print(df.info())#Data ki information print karta hai
+print(df.columns["age"])
