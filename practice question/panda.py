62cc0503e599db76b7dfd3a5ded59f5a5a8120de
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-07 21:23:04 +0530

    add code

--- practice question/panda.py
+++ practice question/panda.py
@@ -5,5 +5,11 @@ df = pd.read_csv("/Users/ahrarmofiz/Downloads/archive (6)/customer_churn_dataset
 #print(df.tail(5))#last 5 rows dikhata hai.
 print(df.shape)#DataFrame me kitni rows aur kitne columns hain.
 print(df.columns)#saare column ke naam
-print(df.info())#Data ki information print karta hai
-print(df.columns["age"])
+df.info()#Data ki information print karta hai
+print(df.describe())#Numeric columns ki summary deta hai.
+print(df["Age"])
+print(df[["Age", "Tenure"]])# multiple columns select krne kay leye
+print(df[df["Age"] > 30])
+print(df[df["Age"] > 30])
+print(df[df["Churn"] == "Yes"])
+print(df[df["Age"] > 50])
