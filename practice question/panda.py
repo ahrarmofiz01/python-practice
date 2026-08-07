a2e1280f9dc2608b3832c118896a3426961b411a
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-07 20:45:25 +0530

    add code

--- practice question/panda.py
+++ practice question/panda.py
@@ -1,4 +1,6 @@
 import pandas as pd
 df = pd.read_csv("/Users/ahrarmofiz/Downloads/archive (6)/customer_churn_dataset-testing-master.csv")
 print(df)
-print(df.head(10))
+print(df.head(10))#top 10 rows
+print(df.tail(5))#last 5 rows dikhata hai.
+print(df.shape)
