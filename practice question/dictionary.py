db58db524bcde3d37e8c151992e3dfeabd0f48d1
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-08 12:29:50 +0530

    add new code

---
+++ practice question/dictionary.py
@@ -0,0 +1,8 @@
+student={"name": "ahrar","age":20,"gender":"male"}
+print(student["age"])
+student["name"]="john"
+student["age"]=21
+student["gender"]="male"
+print(student)
+if "john" in student["name"]:
+    print(True)
