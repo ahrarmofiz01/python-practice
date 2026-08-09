4aafc5a7d82472f704f0e445dbe479b00a7f153e
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-09 21:38:01 +0530

    add code

--- function.py
+++ function.py
@@ -1,11 +1,5 @@
-def find_largest(nums):
-    largest = nums[0]
-
-    for i in nums:
-        if i > largest:
-            largest = i
-
-    return largest
-answer=find_largest([8,9,1021,19])
-print(answer)
+def my_function():
+    return "my name is ahrar mofiz"
+result=my_function()
+print(result)
 
