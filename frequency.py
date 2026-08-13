04996cdda81b19f5f30921fe70f318067497559b
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-13 21:54:43 +0530

    add code

--- frequency.py
+++ frequency.py
@@ -1,8 +1,10 @@
-nums=[1,2,1,3,4,3,9,9,9,9,9,9,9,9,9]
-freq={}
-for i  in nums:
-    if i in freq:
-        freq[i] +=1
-    else:
-        freq[i]=1
-print(freq)
+num=234
+tem=num
+sum_=0
+product=1
+while tem>0:
+    r= tem%10
+    tem//=10
+    sum_+=r
+    product*=r
+print(product-sum_)
