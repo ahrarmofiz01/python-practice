132b6598eb12162ad4b6519a46e9196b55701faf
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-14 21:14:13 +0530

    add code

--- frequency.py
+++ frequency.py
@@ -8,4 +8,16 @@ while tem>0:
     sum_+=r
     product*=r
 print(product-sum_)
-        
+num = 121
+temp = num
+reverse = 0
+
+while temp > 0:
+    r = temp % 10
+    reverse = reverse * 10 + r
+    temp = temp // 10
+
+if num == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
