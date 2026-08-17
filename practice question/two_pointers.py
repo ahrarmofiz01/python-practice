8da271b83c98a7ee1dd9259b067949e08f8585f9
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-17 21:44:47 +0530

    add code

--- practice question/two_pointers.py
+++ practice question/two_pointers.py
@@ -1,4 +1,4 @@
-s = "level"
+s = "mol"
 
 left = 0
 right = len(s) - 1
@@ -12,3 +12,32 @@ while left < right:
     right -= 1
 else:
     print("Palindrome")
+m="level"
+left=0
+right=len(m)-1
+while left<right:
+    if m[left] != m[right]:
+        print("not palindrome")
+        break
+    left +=1
+    right -=1
+else:
+    print("palindrome")
+nums = [0, 0, 2, 3, 5, 7]
+target = 8
+
+left = 0
+right = len(nums) - 1
+
+while left < right:
+    total = nums[left] + nums[right]
+
+    if total == target:
+        print([left, right])
+        break
+
+    elif total < target:
+        left += 1
+
+    else:
+        right -= 1
