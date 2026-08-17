608ff98952ec2beb32edb2f06f3eb75e1568ec34
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-17 21:45:36 +0530

    add code

--- practice question/two_pointers.py
+++ practice question/two_pointers.py
@@ -23,21 +23,3 @@ while left<right:
     right -=1
 else:
     print("palindrome")
-nums = [0, 0, 2, 3, 5, 7]
-target = 8
-
-left = 0
-right = len(nums) - 1
-
-while left < right:
-    total = nums[left] + nums[right]
-
-    if total == target:
-        print([left, right])
-        break
-
-    elif total < target:
-        left += 1
-
-    else:
-        right -= 1
