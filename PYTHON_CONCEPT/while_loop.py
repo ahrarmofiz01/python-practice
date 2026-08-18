d3dab5d5dde6fb3933a72c1a1316bd6e7dbb9951
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-18 14:42:46 +0530

    add code

---
+++ PYTHON_CONCEPT/while_loop.py
@@ -0,0 +1,16 @@
+num=1
+while num<=10:
+    print(num)
+    num=num+1
+list=[0,1,2,3,4,5,6]
+
+i=0
+while i<len(list):
+    print(list[i])
+    i=i+1
+#print 20-1 
+count=20
+while count>=1:
+    print(count)
+    count=count-1
+  
