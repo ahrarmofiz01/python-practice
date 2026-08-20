222a46a4133dd2fda2fa83796b4dd3df3b7b3a47
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-20 21:30:30 +0530

    add code

--- PYTHON_CONCEPT/three_sum.py
+++ PYTHON_CONCEPT/three_sum.py
@@ -1,7 +1,9 @@
-nums = [0, 1, 7, 1, 3]
-target = 9
+nums=[1,1,1,5,9,10]
+target=3
 for i in range(len(nums)):
-    for j in range (i+i,len(nums)):
+    for j in range(i+1,len(nums)):
         for k in range(j+1,len(nums)):
             if nums[i]+nums[j]+nums[k]==target:
                 print([i,j,k])
+
+
