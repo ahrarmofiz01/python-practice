072f0b5ca9aa45898e263786d143c6e7e256f411
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-20 13:08:57 +0530

    add code

--- PYTHON_CONCEPT/for_loop.py
+++ PYTHON_CONCEPT/for_loop.py
@@ -4,13 +4,3 @@ for i in nums:
     if i%2==1:
         count=count+1
 print(count)
-
-num=121
-temp=num
-count=0
-while temp>0:
-    r=temp%10
-    if num%r ==0:
-        count=count+1
-    temp=temp//10
-print(count)
