e6b5962388a578ebaef97926596c25ba1b69c1fe
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-21 11:46:27 +0530

    add code

--- basics_logic_building/.py
+++ basics_logic_building/.py
@@ -19,3 +19,15 @@ while n>0:
     n=n-1
 print(sum)
 #A for loop repeats a specific number of times, while a while loop repeats until a condition changes.
+n=5
+mul=1
+while n>0:
+    mul=n*mul
+    n=n-1
+print(mul)
+n=5
+subs=0
+while n>0:
+    subs=subs-n
+    n=n-1
+print(subs)
