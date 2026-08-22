cfc722a340abd7d96afe8975b8280107892245a3
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-22 21:00:18 +0530

    add code

--- PYTHON_CONCEPT/rock_paper_game.py
+++ PYTHON_CONCEPT/rock_paper_game.py
@@ -4,14 +4,13 @@ c="sisor"
 a>b
 b>c
 c>a
-while True:
-    player1=input("enter your option:")
-    player2=input("enter your option:")
-    if player1>player2:
-        print(player1,"you won")
-    elif player1==player2:
-        print("match draw")
-    else:
-        print("playe2 won")
-        
-        break
+player1=input("enter your option:")
+player2=input("enter your option:")
+if player1>player2:
+    print(player1,"you won")
+elif player1==player2:
+    print("match draw")
+else:
+    print("playe2 won")
+
+    
