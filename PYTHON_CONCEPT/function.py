be3488d44c3565658ace87cb3027a1d1e29d9a13
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-22 14:10:02 +0530

    add code

--- PYTHON_CONCEPT/function.py
+++ PYTHON_CONCEPT/function.py
@@ -3,7 +3,10 @@ def my_function():
 my_function()
 def odd_function(nums):
     for i in nums:
-        if i%2==0:
+        if i%2==1:
             return i
 odd=odd_function([2,4,6,9,7])
 print(odd)
+def math_function(a,b):
+    print(a**2+b**2+2*a*b)
+math_function(2,3)
