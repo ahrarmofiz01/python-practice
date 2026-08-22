9911120dbdfa430514e4bcc54c674d02280d5aa8
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-22 21:04:47 +0530

    add code

--- PYTHON_CONCEPT/rock_paper_game.py
+++ PYTHON_CONCEPT/rock_paper_game.py
@@ -1,16 +1,28 @@
-a="rock"
-b="paper"
-c="sisor"
-a>b
-b>c
-c>a
-player1=input("enter your option:")
-player2=input("enter your option:")
-if player1>player2:
-    print(player1,"you won")
-elif player1==player2:
-    print("match draw")
+player1 = input("Player 1, enter your option: ")
+player2 = input("Player 2, enter your option: ")
+
+if player1 == player2:
+    print("Match draw")
+
+elif player1 == "rock" and player2 == "scissor":
+    print("Player 1 won")
+
+elif player1 == "scissor" and player2 == "paper":
+    print("Player 1 won")
+
+elif player1 == "paper" and player2 == "rock":
+    print("Player 1 won")
+
+elif player2 == "rock" and player1 == "scissor":
+    print("Player 2 won")
+
+elif player2 == "scissor" and player1 == "paper":
+    print("Player 2 won")
+
+elif player2 == "paper" and player1 == "rock":
+    print("Player 2 won")
+
 else:
-    print("playe2 won")
+    print("Invalid option")
 
     
