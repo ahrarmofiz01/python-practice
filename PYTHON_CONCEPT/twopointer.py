703a1abfab8eb7a1d6931e536ea7b43e5418f91c
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-24 19:47:10 +0530

    add code

--- PYTHON_CONCEPT/twopointer.py
+++ PYTHON_CONCEPT/twopointer.py
@@ -6,3 +6,13 @@ while left<right:
         print("word is pelendrome")
     left=left+1
     right=right-1
+num="123"
+left=0
+right=len(num)-1
+while left<right:
+    if num[left]==num[right]:
+        print("num is pelindrome")
+    else:
+        print("not pelendrome")
+    left=left+1
+    right=right-1
