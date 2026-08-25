2afe2b58760c379aedff802fb840c47fee847100
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-25 21:42:02 +0530

    add code

--- PYTHON_CONCEPT/factorial .py
+++ PYTHON_CONCEPT/factorial .py
@@ -5,4 +5,9 @@ while count<=num:
     factorial=factorial*count
     count=count+1
 print(factorial)
+factorial=1 
+for i in range(1,6):
+    factorial=factorial*i
+    i=i+1
+print(factorial)
     
