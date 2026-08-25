4c0887dacbe8668142897789fb20918c5688415c
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-25 21:43:30 +0530

    add comment

--- PYTHON_CONCEPT/factorial .py
+++ PYTHON_CONCEPT/factorial .py
@@ -1,3 +1,4 @@
+#factorial with while loop
 num=5
 count=1
 factorial=1
@@ -6,8 +7,8 @@ while count<=num:
     count=count+1
 print(factorial)
 factorial=1 
-for i in range(1,6):
+for i in range(1,7):
     factorial=factorial*i
     i=i+1
 print(factorial)
-    
+#with for loop
