428510ce38cb946c969b8f64bf0ff10bd0a9b4f3
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-26 21:01:31 +0530

    add code

--- basics_logic_building/First Repeating Number.py
+++ basics_logic_building/First Repeating Number.py
@@ -5,3 +5,14 @@ for i in list:
         print(i)
         break
     seen[i]=True
+    nums=[1,1,1,2,2,3,4,5,5,6,6]
+    freq = {}
+
+for i in nums:
+
+    if i in freq:
+        freq[i] = freq[i] + 1
+    else:
+        freq[i] = 1
+
+print(freq)
