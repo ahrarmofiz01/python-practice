e59d749f631c6073ece2f9613bfed697772e9e89
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-26 20:39:36 +0530

    add code

--- basics_logic_building/.py
+++ basics_logic_building/.py
@@ -1,7 +1,21 @@
-list=[23,25,27,26,28,30,32,31,35]
-largest=0
-for i in list:
-    if i>largest:
-        largest=i
+temps = [23, 25, 27, 26, 28, 30, 32, 31, 35]
+
+current = 1
+largest = 1
+
+for i in range(1, len(temps)):
+
+    if temps[i] > temps[i - 1]:
+        current = current + 1
+
+        if current > largest:
+            largest = current
+
+    else:
+        current = 1
+
 print(largest)
+
+    
+
     
