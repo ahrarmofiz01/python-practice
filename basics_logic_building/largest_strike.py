b50dd8c27b2a3a423080a2160c519dfee9e0db93
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-27 20:31:49 +0530

    add code

--- basics_logic_building/largest_strike.py
+++ basics_logic_building/largest_strike.py
@@ -3,7 +3,7 @@ score=[0,1,2,1,2,3,4,5,4,2]
 current=1
 largest=1
 for i  in range(1,len(score)):
-    if score[i]>score[i-1]:
+    if score[i]>score[i-1] :
         current=current+1
         if current>largest:
             largest=current
