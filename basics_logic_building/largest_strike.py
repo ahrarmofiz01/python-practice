8daab00c8eec82471d503ee3a61f32c537cd0a50
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-27 20:16:38 +0530

    add code

--- basics_logic_building/largest_strike.py
+++ basics_logic_building/largest_strike.py
@@ -1,20 +1,14 @@
-#longest increasing streak
-temps = [23, 25, 27, 26, 28, 30, 32, 31, 35]
-
-current = 1
-largest = 1
-
-for i in range(1, len(temps)):
-
-    if temps[i] > temps[i - 1]:
-        current = current + 1
-
-        if current > largest:
-            largest = current
-
+#longest strike
+score=[0,1,2,1,2,3,4,5,4,2]
+current=1
+largest=1
+for i  in range(1,len(score)):
+    if score[i]>score[i-1]:
+        current=current+1
+        if current>largest:
+            largest=current
     else:
-        current = 1
-
+        current=1
 print(largest)
 
     
