9f029a76378b8af4afb9358f4a24410d6f85fdd0
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-28 20:49:01 +0530

    add code

--- basics_logic_building/First Repeating Number.py
+++ basics_logic_building/First Repeating Number.py
@@ -1,10 +1,3 @@
-list=[1,2,3,4,5,1]
-seen={}
-for i in list:
-    if i in seen:
-        print(i)
-        break
-    seen[i]=True
 nums=[1,1,1,2,2,3,4,5,5,6,6]
 
 freq = {}
--- basics_logic_building/largest_strike.py
+++ basics_logic_building/largest_strike.py
@@ -1,16 +1,12 @@
-#longest strike
-score=[0,1,2,1,2,3,4,5,4,2]
+#lonest strike
+nums=[1,2,3,2,3,4,5,6,5,7]
 current=1
 largest=1
-for i  in range(1,len(score)):
-    if score[i]>score[i-1] :
+for i in range(1,len(nums)):
+    if nums[i]>nums[i-1]:
         current=current+1
         if current>largest:
             largest=current
     else:
         current=1
 print(largest)
-
-    
-
-    
