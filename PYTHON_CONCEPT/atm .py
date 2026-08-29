35d0c1caddad37619b0e1736dd30b002a7e8c0f3
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-29 14:15:36 +0530

    add code

--- PYTHON_CONCEPT/atm .py
+++ PYTHON_CONCEPT/atm .py
@@ -1,8 +1,26 @@
-avlbal=10000
-withdraw=int(input("enter your ammount:"))
-if withdraw<=avlbal:
-   print("withdraw is completed collect your cash")
-else:
-    print("write current amount")
+avlbal = 10000 
+code = 1234 
+
+
+pin = int(input("Enter your pin: ")) 
+
+while True: 
+    if pin == code: 
+        
+        withdraw = int(input("Enter your amount: ")) 
+        
+        if withdraw <= avlbal: 
+            print("Collect your cash") 
+            avlbal -= withdraw 
+            break 
+        else: 
+            print("Insufficient balance") 
+            break 
+    else: 
+        print("Incorrect pin")
+        
+        pin = int(input("Enter your pin: ")) 
+
+            
     
     
