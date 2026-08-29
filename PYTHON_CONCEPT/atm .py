87b9d981bd1c48e305007854261a0755615faa5d
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-29 13:58:50 +0530

    correction

--- PYTHON_CONCEPT/atm .py
+++ PYTHON_CONCEPT/atm .py
@@ -1,8 +1,8 @@
 avlbal=10000
-while True:
-    withdraw=int(input("enter your ammount:"))
-    if withdraw<=avlbal:
-        print("withdraw is completed collect your cash")
-        break
-    else:("write current amount")
+withdraw=int(input("enter your ammount:"))
+if withdraw<=avlbal:
+   print("withdraw is completed collect your cash")
+else:
+    print("write current amount")
+    
     
