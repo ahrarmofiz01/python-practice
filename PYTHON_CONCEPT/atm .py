2b5541a9db0b767a2891b5e7465a0dfcaaf4a60c
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-29 13:54:09 +0530

    new question

---
+++ PYTHON_CONCEPT/atm .py
@@ -0,0 +1,8 @@
+avlbal=10000
+while True:
+    withdraw=int(input("enter your ammount:"))
+    if withdraw<=avlbal:
+        print("withdraw is completed collect your cash")
+        break
+    else:("write current amount")
+    
