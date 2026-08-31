e53ea912ce673d5ed58f78587297a21059c2dad8
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-31 21:11:37 +0530

    add code

---
+++ PYTHON_CONCEPT/oops.py
@@ -0,0 +1,11 @@
+class myclass:
+    x=5
+p1=myclass
+print(p1.x)
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+p1=person("ahrar",36)
+print(p1.name)
+print(p1.age)
