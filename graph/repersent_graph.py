4b5b2213b6918f6d4ec057c735abe5ec30502f53
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-01 14:04:45 +0530

    add code

--- graph/repersent_graph.py
+++ graph/repersent_graph.py
@@ -2,3 +2,14 @@ v=6#number of nodes
 e=7#number of edges
 edges=[(0,1),(0,3),(0,4),(1,2),(1,5),(2,4),(3,4)]#edges
 print(v,e,edges)
+#adjancy list
+adjlist=[]
+for i in range(v):
+    adjlist.append([])
+for edge in edges:
+    x=edge[0]
+    y=edge[1]
+    adjlist[x].append(y)
+    adjlist[y].append(x)
+for i in range(v):
+    print(i,"->",adjlist[i])
