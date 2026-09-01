54350eeb2a4bf741878b85d3b8a1459ab1dafd52
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-01 20:32:00 +0530

    add code

--- graph/repersent_graph.py
+++ graph/repersent_graph.py
@@ -24,3 +24,24 @@ for edge in edges:
     adjmatrix[y][x]=1
 for i in adjmatrix:
     print(i)
+from collections import deque
+
+# BFS
+q = deque()
+visited = [False] * v
+ans = []
+
+q.append(0)
+visited[0] = True
+ans.append(0)
+
+while len(q) > 0:
+    front = q.popleft()
+
+    for x in adjlist[front]:
+        if not visited[x]:
+            q.append(x)
+            visited[x] = True
+            ans.append(x)
+
+print(ans)
