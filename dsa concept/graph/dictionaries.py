9895cbe95571a592290d574f95409bd3d6985487
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-02 14:16:46 +0530

    hasmap question

--- dsa concept/graph/dictionaries.py
+++ dsa concept/graph/dictionaries.py
@@ -7,3 +7,12 @@ dict.update({4:"hello",5:"hi"})
 print(dict)
 for i in dict:
     print( i,dict[i])
+#### frequency question#########
+list1=[1,2,3,4,5,6,1]
+freq={}
+for i in list1:
+    if i in freq:
+        freq[i]=freq[i]+1
+    
+print(freq)
+    
