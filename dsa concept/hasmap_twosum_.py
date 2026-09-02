1adc899ca28f84aa76e43635553b65ff218ed596
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-02 14:54:00 +0530

    add code

--- dsa concept/hasmap_twosum_.py
+++ dsa concept/hasmap_twosum_.py
@@ -1,12 +1,13 @@
-# 1. Rename the variable to avoid using the built-in 'list' keyword
-numbers = [2, 3, 10, 15, 8]
-target = 5
-freq = {}
+number = [1, 2, 5, 6, 9]
+target = 3
 
-for i in numbers:
-    r = target - i
-    if r in freq:
-        print([r, i])
-    
-    # 2. Add the current number to the dictionary so future iterations can find it
-    freq[i]=True
+seen = {}
+
+for i in number:
+
+    required = target - i
+
+    if required in seen:
+        print(seen[required], i)
+
+    seen[i] = i
