9345a985a0f88cac0f5be61cdcff5b6bef09fd3e
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-03 20:56:00 +0530

    add code

--- dsa concept/sliding_window.py
+++ dsa concept/sliding_window.py
@@ -1,6 +1,13 @@
 list=[1,2,3,4,5]
 k=3
 sum=sum(list[:k])
-for i in range(k,len(list)):
-    sum=sum+list[i]-list[i-k]
-print(sum)
+max_sum=sum
+average=sum/k
+max_average=average
+for i in range(len(list)-k):
+    sum=sum-list[i]+list[i+k]
+    max_sum=max(max_sum,sum)
+    max_average=max(max_average,sum/k)
+    print(max_average)
+
+    
