bd183da1ee83086f4fdc3b7520f710f87d7624d0
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-04 12:15:50 +0530

    experement with code

--- dsa concept/sliding_window.py
+++ dsa concept/sliding_window.py
@@ -1,9 +1,3 @@
 list=[2,4,6,8,10,12]
-k=3
-n=len(list)
-print(n)
-sum=0
-for i in range(k,n):
-    sum=sum+list[i]
-    sum=sum-list[i-k]
-print(sum)
+for i in range(len(list)):
+    print(i,list[i])
