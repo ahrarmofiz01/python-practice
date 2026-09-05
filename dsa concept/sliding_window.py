b35a0a97880ed7af42c484151bc308e780ab9074
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-05 15:24:37 +0530

    rewrite answer

--- dsa concept/sliding_window.py
+++ dsa concept/sliding_window.py
@@ -1,8 +1,17 @@
-list=[]
-for i in range(1,11):
-    print(i)
-    list.append(i)
-print(list)
+list=[1,2,3,4,5,6,7]
+k=3
+n=len(list)
+sum=0
+for i in range(k):
+    sum=sum+list[i]
+    print(sum)
+ans=sum/k
+for i in range(k,n):
+    sum=sum+list[i]
+    sum= sum-list[i-k]
+    ans=max(sum/k,ans)
+print(ans)
+
    
 
 
