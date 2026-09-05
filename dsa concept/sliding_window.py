66f49ae0978acfdd52b5639badcc7e5cc5ea0cf4
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-05 12:45:01 +0530

    add code

--- dsa concept/sliding_window.py
+++ dsa concept/sliding_window.py
@@ -1,15 +1,8 @@
-list=[2,4,6,8,10,12]
-k=3
-n=len(list)
-sum=0
-for i in range(k):
-    sum=sum+list[i]
-    print(sum)
-ans=sum
-for i in range(k,n):
-    sum=sum+list[i]
-    sum=sum-list[i-k]
-    ans=max(ans,sum)
+list=[]
+for i in range(1,11):
+    print(i)
+    list.append(i)
+print(list)
    
 
 
