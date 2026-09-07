42ea358e07d68500ad2e4654657edf3178fe1514
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-07 12:34:28 +0530

    add code

--- dsa concept/recurtion/npower.py
+++ dsa concept/recurtion/npower.py
@@ -1,5 +1,18 @@
+#with loop
 def poweroftwo(n):
+    if n==0:
+        return False
     while n%2==0:
         n=n/2#n/=2 v likh skte hai 
     return n==1
-print(poweroftwo(16))
+print(poweroftwo(0))
+# with recurtion
+def poweroftwo(n):
+    if n==0:
+        return False
+    if n==1:
+        return True
+    if n%2==1:
+        return False
+    return poweroftwo(n//2)
+print(poweroftwo(8))
