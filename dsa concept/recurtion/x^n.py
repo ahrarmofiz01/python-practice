9b076d165666fed3456703ffb2e90d5f2cbb773d
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-07 19:35:21 +0530

    add code

---
+++ dsa concept/recurtion/x^n.py
@@ -0,0 +1,17 @@
+def mypow(x,n):
+    if n==0  :
+        return 1
+    if n<0:
+        n*=-1
+        return 1/mypow(x,n)
+   
+    if n==1:
+        return x
+    a=mypow(x,n//2)
+    if n%2==1:
+        return a*a*x
+    else:
+        return a*a
+    
+
+print(mypow(2,-2))
