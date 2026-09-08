d66263fc09be623ba8c606ee984864b6293e87c1
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-08 14:56:22 +0530

    ad code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,13 +1,8 @@
-n=[1,2,3,4,5]
-k=2
-p=len(n)
-print(p)
-sum=0
-for i in range(k):
-    sum=sum+n[i]
-ans=sum/2
-for i in range(k,p):
-    sum= sum+n[i]
-    sum=sum-n[i-k]
-    ans=max(ans,sum/2)
-print(ans)
+num=[1,2,9,18,20]
+target=38
+seen={}
+for i in num:
+    require=target-i
+    if require in seen:
+        print(require,i)
+    seen[i]=1
