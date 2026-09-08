fce230c14c0115f22f45242e614fce23142fb48c
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-08 14:33:10 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,9 +1,9 @@
-#def prime(n):
-    #if n==0 or n==1:
-     #   return False
-    #for i in range(2, int(n**0.5) + 1):
-       # if n%i==0:
-          #  return False
-    #return True
-#print(prime(11))
-
+def power(n):
+    if n==0:
+        return False
+    if n==1:
+        return True
+    if n%2==1:
+        return False
+    return power(n//2)
+print(power(8))
