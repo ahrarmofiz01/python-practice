c1ba13b7e1cc8eaf0d52a0b2ce63aa0b0b923de4
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-09 13:05:28 +0530

    add code

--- dsa concept/heap.py
+++ dsa concept/heap.py
@@ -11,3 +11,19 @@ if len(stone)==1:
     print(stone[0])
 else:
     print("no stone weight" , 0)
+#with munctiom
+import heapq
+def stone(list):
+    heap=[]
+    for i in list:
+        heapq.heappush(heap,-i)
+    while len(heap)>1:
+        a=-heapq.heappop(heap)
+        b=-heapq.heappop(heap)
+        if a!=b:
+            heapq.heappush(heap,-(a-b))
+    if len(heap)>0:
+        return -heap[0]
+    else:
+        return 0
+print(stone([1,2,3,4,5,6]))
