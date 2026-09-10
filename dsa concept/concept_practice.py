54443bc9196d6694948700dbb4b2e75036fb6766
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-10 20:13:38 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -2,9 +2,11 @@
 def pow(n):
     if n==1:
         return True
+    if n==0:
+        return False
     if n%2==1:
         return False
   
     return pow(n//2)
-print(pow(64))
+print(pow(16))
     
