9c66412b3e6b1c3dd9d994ef861774406be63aae
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-10 20:10:06 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,6 +1,10 @@
-def num(n):
-    if n==10:
-        return 
-    print(n)
-    num(n+1)
-num(0)
+#power of n
+def pow(n):
+    if n==1:
+        return True
+    if n%2==1:
+        return False
+  
+    return pow(n//2)
+print(pow(64))
+    
