4a3007ae89b70a2629fdf52b2558cd86d31a6395
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-11 14:18:59 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,8 +1,11 @@
 #concept practice
 def num(n):
-    if n==-5:
-        return n
-    print(n)
-    num(n-1)
-print(num(5))
+    if n==0:
+        return False
+    if n==1:
+        return True
+    if n%2==1:
+        return False
+    return  num(n//2)
+print(num(2))
   
