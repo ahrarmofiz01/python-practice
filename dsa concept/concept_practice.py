05b7eddd3de3f38c4924ad33c6545f2aaf003c86
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-12 14:12:52 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,23 +1,9 @@
-#concept practice
-def pow(n):
-    if n==0:
-        return False
-    if n==1:
-        return True
-    if n%2==1:
-        return False
-    return pow(n//2)
-print(pow(8))
-#sliding window question
-list=[1,2,3,4,5]
-n=len(list)
-k=2
-sum=0
-for i in range(k):
-    sum=sum+list[i]
-print(sum)
-for i in range(k,n):
-    sum=sum+list[i]
-    sum= sum-list[i-k]
-print(sum)
+#second largest number
+array=[0,5,7,2,9,8]
+largest=array[0]
+for i in array:
+    if i>largest:
+        largest=i
+print(largest)
+        
   
