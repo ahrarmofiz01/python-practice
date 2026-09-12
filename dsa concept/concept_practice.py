b31da9696a2ba1ab086b54de888d7a9019f797e0
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-12 21:16:51 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,18 +1,9 @@
-array = [0, 5, 7, 2, 9, 8]
-
-largest = array[0]
-second = array[1]
-
+array=[0,8,2,7,9]
+largest=array[0]
+second=array[1]
 for i in array:
-
-    if i > largest:
-        second = largest
-        largest = i
-
-    elif i > second:
-        second = i
-
-print("Largest:", largest)
-print("Second Largest:", second)
-        
+    if i>largest:
+        second=largest
+        largest=i
+print(second)
   
