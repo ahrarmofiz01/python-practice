d6423db2440e81123254240b617c76804b8dae13
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-12 14:40:41 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,9 +1,18 @@
-#second largest number
-array=[0,5,7,2,9,8]
-largest=array[0]
+array = [0, 5, 7, 2, 9, 8]
+
+largest = array[0]
+second = array[1]
+
 for i in array:
-    if i>largest:
-        largest=i
-print(largest)
+
+    if i > largest:
+        second = largest
+        largest = i
+
+    elif i > second:
+        second = i
+
+print("Largest:", largest)
+print("Second Largest:", second)
         
   
