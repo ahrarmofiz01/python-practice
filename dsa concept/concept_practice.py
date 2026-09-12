688a973c7bd37cdc09835e069216f0809f74d5cd
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-12 13:21:17 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -8,4 +8,16 @@ def pow(n):
         return False
     return pow(n//2)
 print(pow(8))
+#sliding window question
+list=[1,2,3,4,5]
+n=len(list)
+k=2
+sum=0
+for i in range(k):
+    sum=sum+list[i]
+print(sum)
+for i in range(k,n):
+    sum=sum+list[i]
+    sum= sum-list[i-k]
+print(sum)
   
