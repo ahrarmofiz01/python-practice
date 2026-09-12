a497dc9821fb8480b1fc88ba37271411f5611d13
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-12 12:34:45 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,7 +1,11 @@
 #concept practice
-def feb(n):
-    if n==0 or n==1:
-        return n
-    return feb(n-1)+feb(n-2)
-print(feb(4))
+def pow(n):
+    if n==0:
+        return False
+    if n==1:
+        return True
+    if n%2==1:
+        return False
+    return pow(n//2)
+print(pow(8))
   
