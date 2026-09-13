bec1d17f394fc18cd454643976fbfee0e921cbdb
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-13 13:28:59 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,8 +1,9 @@
-array = [2,3,3,4,2,8,9,3,7,2,2,2,2,2,2,2,22,2]
-seen = {}
+array=[8,0,2,9,29,0,0,2,1.23,4]
+seen={}
 for i in array:
-    if i in seen:
-     seen[i] += 1
+    if i in  seen:
+        seen[i]=seen[i]+1
+
     else:
-       seen[i] = 1
+        seen[i]=1
 print(seen)
