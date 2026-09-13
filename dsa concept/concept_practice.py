0f42c19fe47f315f47ef44ee24f389712617c26e
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-13 21:43:21 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -21,4 +21,24 @@ print("Recommended movies:")
 for movie in recommended:
     print(movie)
 
+users = {
+    "A": ["Inception", "Avatar", "Titanic"],
+    "B": ["Avatar", "Titanic", "Interstellar"],
+    "C": ["Inception", "Interstellar", "Avengers"]
+}
 
+user = "A"
+
+watched = set(users[user])
+recommended = set()
+
+for other_user in users:
+    if other_user != user:
+        for movie in users[other_user]:
+            if movie not in watched:
+                recommended.add(movie)
+
+print("Recommended movies:")
+
+for movie in recommended:
+    print(movie) 
