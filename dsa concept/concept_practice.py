59fb3df24aca3f7744b1ebb0d89b391f72c6a908
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-13 13:15:42 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -1,8 +1,8 @@
-array=[2,3,3,4,2,8,9,3,7,2,2,2,2,2,2,2,22,2]
-count=0
-num=array[0]
+array = [2,3,3,4,2,8,9,3,7,2,2,2,2,2,2,2,22,2]
+seen = {}
 for i in array:
-    if num==i:
-        count= count+1
-print(num,"frequency = ",count)
-   
+    if i in seen:
+     seen[i] += 1
+    else:
+       seen[i] = 1
+print(seen)
