c448cf3a953e84d2c754d8ced7562091e9dc01bf
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-14 20:40:22 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -5,4 +5,11 @@ def num(n):
     num(n+1)
 num(1)
 
- 
+numbers = [4, 2, 4, 3, 2, 4, 5, 3, 2, 2, 5, 5, 5]
+seen={}
+for i in numbers:
+    if i in seen:
+        seen[i]=seen[i]+1
+    else:
+        seen[i]=1
+print(seen)
