9fa2ab86c52802c72e78efe1fdea2c723748037a
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-14 21:03:40 +0530

    add code

--- dsa concept/concept_practice.py
+++ dsa concept/concept_practice.py
@@ -5,11 +5,19 @@ def num(n):
     num(n+1)
 num(1)
 
-numbers = [4, 2, 4, 3, 2, 4, 5, 3, 2, 2, 5, 5, 5]
+numbers = [7, 2, 7, 3, 2, 7, 4, 3, 2, 2, 5, 4, 7]
 seen={}
 for i in numbers:
     if i in seen:
         seen[i]=seen[i]+1
+        
     else:
         seen[i]=1
 print(seen)
+largest=0
+largest_key=None
+for kew ,value in seen.items():
+    if value>largest:
+        largest=value
+        largest_key=kew
+print(largest_key,largest)
