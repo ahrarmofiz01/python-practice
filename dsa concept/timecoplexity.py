335b30ad139b0db0649ef621919f58e785e36078
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-16 19:28:11 +0530

    add code

--- dsa concept/timecoplexity.py
+++ dsa concept/timecoplexity.py
@@ -3,3 +3,10 @@ for i  in range(10):
         print(i,j)
 for i in range(10*2):
     print(i)
+# constant time complexity
+n=100000
+m=500000
+#o(1)
+for i in range(4):
+    for j in range(3):
+        print(n,m,i,j, end=" ")
