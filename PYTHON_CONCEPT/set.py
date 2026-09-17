cd240c0103ffe7d6c75cf9fc0d267297f124ff75
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-17 20:41:00 +0530

    add code

--- PYTHON_CONCEPT/set.py
+++ PYTHON_CONCEPT/set.py
@@ -6,3 +6,15 @@ print(len(set1))
 print(type(set1))
 set1.add("hello world")
 print(set1)
+
+set2=set1.copy()
+print(set2)
+set2.add("name")
+print(set2)
+set2.discard(100)
+print(set2)
+set1={1,3,5,8,9}
+set2={9,5,11,28,1}
+print(set1|set2)
+#or
+print(set1.union(set2))
