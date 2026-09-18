37931599c10c7a04a44e6f04738183a2e0e79468
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-18 19:44:51 +0530

    dictionaries code

---
+++ PYTHON_CONCEPT/dictionaries.py
@@ -0,0 +1,13 @@
+# a python dictionary is a collection of items ,simillar to lists and tuples.however is a key -value pair 
+dict1={}
+print(type(dict1))
+dict1={1:"ahrar",
+       2:"mohan",
+       3:"aadil",
+       4: "rahul"
+
+
+
+       }
+print(len(dict1))
+dict1
