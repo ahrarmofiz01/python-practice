61b9d3c1de16e25e1577e83ac53dfa14c0bba9d6
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-20 21:14:30 +0530

    add code

---
+++ dsa concept/substring.py
@@ -0,0 +1,20 @@
+#LONGEST SUBSTRING WITHOUT REPEARTING CHARACTERS
+s="abcabcdab"
+n = len(s)
+
+if n <= 1:
+    print(n)
+else:
+    set1 = set()
+    i, j = 0, 0  
+    ans = 0
+
+    while j < n:
+        while s[j] in set1:
+            set1.remove(s[i])
+            i += 1
+        set1.add(s[j])
+        ans = max(ans, j - i + 1)
+        j += 1
+
+    print(ans)
