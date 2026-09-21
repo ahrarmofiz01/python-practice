48bf87a22c59ccc526a9376d66863258a24439a6
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-21 12:16:40 +0530

    add code

--- dsa concept/kadane's.py
+++ dsa concept/kadane's.py
@@ -1,12 +1,14 @@
 #MAXIMUM SUBARRAY
-nums=[2,-3,1,-3,7,0,-9,-7,-1]
+nums=[-2,1,-3,4,-1,2,1,-5,4]
 n=len(nums)
 ans=nums[0]
 current_sum=0
 for i in nums:
     current_sum+=i
+   
     if current_sum>ans:
         ans=current_sum
     if current_sum<0:
         current_sum=0
+   
 print(ans)
