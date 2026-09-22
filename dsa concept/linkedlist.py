b31e54c534fc92f46adcfb6270861b89ea38b3ef
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-22 21:47:50 +0530

    add code

--- dsa concept/linkedlist.py
+++ dsa concept/linkedlist.py
@@ -11,3 +11,15 @@ a.next=b
 b.next=c
 
 print(head.next.next.data)
+def linkll(head):
+    curr=head
+    while curr !=None:
+        print(curr.data)
+        curr=curr.next
+a=node(2)
+b=node(3)
+c=node(4)
+a.next=b
+b.next=c
+head=a
+linkll(head)
