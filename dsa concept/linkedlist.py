a0d4a34c660100bbf09f79bf61b423703d990208
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-23 20:20:51 +0530

    add code

--- dsa concept/linkedlist.py
+++ dsa concept/linkedlist.py
@@ -3,12 +3,15 @@ class node:
     def __init__(self,data):
         self.data=data
         self.next=None
+        self.prev=None
 a=node(1)
 b=node(2)
 c=node(3)
 head=a
 a.next=b
+b.prev=a
 b.next=c
+c.prev=a
 
 print(head.next.next.data)
 def linkll(head):
