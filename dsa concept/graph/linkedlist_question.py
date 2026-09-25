43ff5c36e0a859b1ae3230cb3efeff929efccbdd
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-25 11:27:16 +0530

    insert node

--- dsa concept/graph/linkedlist_question.py
+++ dsa concept/graph/linkedlist_question.py
@@ -18,26 +18,11 @@ b=node(2)
 c=node(3)
 a.next=b
 b.next=c
-head=a
 newnode=node(0)
 newnode.next=head
 head=newnode
-#insert at endnode
-newnode=node(5)
-curr=head
-while curr.next !=None:
-    curr=curr.next
-curr.next=newnode
-#insert at k th term
-k=4
-curr =head
-newnode=node(7)
-for i in range(k-2):
-    curr=curr.next
-newnode.next=curr.next
-curr.next=newnode
 
-
+   
 linkedl(head)
 
            
