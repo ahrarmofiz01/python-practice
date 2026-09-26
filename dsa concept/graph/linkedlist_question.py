eb3bfcde56d821402627ecb83ed08ab3b762706b
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-09-26 21:12:00 +0530

    add code

--- dsa concept/graph/linkedlist_question.py
+++ dsa concept/graph/linkedlist_question.py
@@ -1,44 +1,23 @@
 class node:
     def __init__(self,data):
-        self.data=data
+        self .data=data
         self.next=None
 a=node(1)
 b=node(2)
 c=node(3)
+d=node(4)
+head=a
 a.next=b
 b.next=c
-head=a
-def linkedl(head):
+c.next=d
+
+def linkidl(head):
     curr=head
     while curr != None:
         print(curr.data)
         curr=curr.next
-a=node(1)
-b=node(2)
-c=node(3)
-a.next=b
-b.next=c
-#insert at the first point
-newnode=node(0)
-newnode.next=head
-#delete add the  first point
-head=newnode
-head=head.next
-curr=head
-newnode=node(5)
-while curr.next.next != None:
-    curr=curr.next
-curr.next=newnode
-newnode=curr.next
-curr=head
-curr=head
-while curr.next.next!=None:
-    curr=curr.next
-curr.next=None
+linkidl(head)
 
-
-   
-linkedl(head)
 
            
 
